6e2f6cd15de0360f82a3abb4d03698185c55e320
brcevssl <brcevssl@gmail.com>
2024-05-27 17:29:06 +0200

    Ajout du controller employee et de la fonction post

--- employee/src/main/java/com/bricevassail/employee/service/EmployeeService.java
+++ employee/src/main/java/com/bricevassail/employee/service/EmployeeService.java
@@ -1,8 +1,20 @@
 package com.bricevassail.employee.service;
 
+import com.bricevassail.employee.entity.Employee;
+import com.bricevassail.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee employee){
+        System.out.println("EmployeeService created. employeeRepository: " + employeeRepository);
+        return employeeRepository.save(employee);
+    }
+
 }
 
