697ea4f3f950d2ae803681ce553cecab68770ddd
brcevssl <brcevssl@gmail.com>
2024-05-28 01:39:07 +0200

    Test job codecov

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,16 @@
+import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <h1 class="text-center">CRM</h1>
-  );
-}
-
+function App () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit src/App.js and save to reload. 
+          </p> 
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> 
+        </header> 
+      </div> ); 
+} 
 export default App;
