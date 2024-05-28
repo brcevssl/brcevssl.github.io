2a44d3ce6971b7dc4b1b320f94d1f5a2d996fec3
brcevssl <brcevssl@gmail.com>
2024-05-28 22:03:40 +0200

    Modification du test

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('Pipeline', () => {
-  render(<App/>);
+  render(<App />);
   const linkElement = screen.getByText('Employee');
   expect(linkElement).toBeInTheDocument();
 });
